b3c0fd84b671b2995a973c34d56bb31ff35075cc
P-Kadam <kadam.pr22@gmail.com>
2025-07-11 13:07:05 -0700

    Completed 10.13.2
    10.13.2 - Add an alarm feature to the Clock class of Programming Project 10.13.1. When set_alarm(hours, minutes) is called, the clock stores the alarm. When you call get_time, and the alarm time has been reached or exceeded, return the time followed by the string "Alarm" (or, if you prefer, the string "\u23F0") and clear the alarm. What do you need to do to make the set_alarm function work for WorldClock objects?

--- 10.13.1/10.13.1/main.cpp
+++ 10.13.1/10.13.1/main.cpp
@@ -18,7 +18,18 @@ class Clock
 public:
     virtual int get_hours();
     int get_minutes();
+    
+    //OVERRIDE: get_time() for 10.13.2
     string get_time();
+    
+    //ADDED 10.13.2
+    void set_alarm(int hours, int minutes);
+    
+protected:
+    vector<int> alarm;
+    
+    //checks whether alarm has gone off at current time
+    bool alarm_ring();
 };
 
 int Clock::get_hours()
@@ -48,8 +59,7 @@ int Clock::get_minutes()
 string Clock::get_time()
 {
     string minutes = to_string(get_minutes());
-    
-    
+
     if(minutes.size() == 1)
     {
         minutes.insert(0, 1, '0'); //formats minutes less than 10
@@ -58,13 +68,50 @@ string Clock::get_time()
     {
         minutes.insert(0, 2, '0'); //formats the minutes when clock strikes a new hour
     }
-    
-    
+        
     string time = to_string(get_hours()) + ":" + minutes;
+    
+    if(alarm_ring() && !alarm.empty())
+    {
+        cout << "Alarm!" << endl;
+        alarm.clear();
+    }
+    
     return time;
 }
 
 
+void Clock::set_alarm(int hours, int minutes)
+{
+    alarm.push_back(hours);
+    alarm.push_back(minutes);
+    
+}
+
+
+bool Clock::alarm_ring()
+{
+    int hr = get_hours();
+    int mins = get_minutes();
+    
+    //conditions for alarm to ring:
+    //current hour > alarm hour
+    //current hour == alarm hour && current minutes == alarm minutes
+    //curent hour == alarm hour && current minutes > alarm minutes
+    
+    if(alarm.empty())
+    {
+        return false;
+    }
+
+    if( (hr > alarm[0]) || (hr == alarm[0] && mins >= alarm[1]) )
+    {
+        //alarm is ringing
+        return true;
+    }
+    
+    return false;
+}
 
 //DERIVED CLASS
 class WorldClock : public Clock
@@ -72,6 +119,8 @@ class WorldClock : public Clock
 public:
     WorldClock(int userOffset);
     int get_hours() override;
+//    void set_alarm(int hours, int minutes);
+    
 private:
     int timeOffset;
 };
@@ -102,9 +151,13 @@ int main(int argc, const char * argv[]) {
     
     Clock c1;
     
-    cout << c1.get_time() << endl;
+    c1.set_alarm(12, 54);
+//    cout << c1.get_time() << endl;
     
     Clock* NY = new WorldClock(3);
     
+    NY->set_alarm(4, 2);
+    
     cout << NY->get_time() << endl;
+
 }
